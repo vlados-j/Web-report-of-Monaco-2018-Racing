2edfa70b635c389a792aac0c3bd8be3517f901a1
Vladyslav Dzhulai <dzhulai.vladyslav@gmail.com>
2022-10-18 18:18:18 -0400

    Changed the logic in main.py and added some changes to templated

--- main.py
+++ main.py
@@ -1,78 +1,48 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from application_vlados import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+def index():
+    return redirect(url_for('report'))
+
+
 @app.route('/report/')
 def report():
+    args = request.args
     structured_info = processing_data('files/start.log', 'files/end.log', 'files/abbreviations.txt')
-    prepared_info_for_report = build_report(structured_info, 'asc')
-    number_of_valid_racers = len([None for racer in prepared_info_for_report if racer.lap_time])
-    racer_number_sequence = iter(range(1, number_of_valid_racers + 1))
-    info_for_output = []
-    for racer in prepared_info_for_report:
-        if racer.lap_time:
-            racers_number = next(racer_number_sequence)
-            info_for_output.append((racers_number, racer.name, racer.team, racer.lap_time_str))
-        else:
-            info_for_output.append(('-', racer.name, racer.team, racer.lap_time_str))
-    return render_template('report.html', info_for_output=info_for_output)
-
-
-@app.route('/report/desc')
-def report_desc():
-    structured_info = processing_data('files/start.log', 'files/end.log', 'files/abbreviations.txt')
-    prepared_info_for_report = build_report(structured_info, 'desc')
-    number_of_valid_racers = len([None for racer in prepared_info_for_report if racer.lap_time])
-    racer_number_sequence = iter(range(number_of_valid_racers, 0, -1))
-    info_for_output = []
-    for racer in prepared_info_for_report:
-        if racer.lap_time:
-            racers_number = next(racer_number_sequence)
-            info_for_output.append((racers_number, racer.name, racer.team, racer.lap_time_str))
-        else:
-            info_for_output.append(('-', racer.name, racer.team, racer.lap_time_str))
-    return render_template('report_desc.html', info_for_output=info_for_output)
+    prepared_info_for_report = info_for_output(structured_info, args.get("order"))
+    return render_template('report.html', prepared_info_for_report=prepared_info_for_report, order=args.get("order"))
 
 
 @app.route('/report/drivers/')
 def drivers():
+    args = request.args
     structured_info = processing_data('files/start.log', 'files/end.log', 'files/abbreviations.txt')
-    prepared_info_for_report = build_report(structured_info, 'asc')
+    if args.get("abbreviation"):
+        for racer in structured_info.values():
+            if racer.abbreviation == args.get("abbreviation"):
+                return render_template('driver.html', racer=racer)
+
+    prepared_info_for_report = info_for_output(structured_info, args.get("order"))
+    return render_template('drivers.html', prepared_info_for_report=prepared_info_for_report, order=args.get("order"))
+
+
+def info_for_output(structured_info, ordering):
+    prepared_info_for_report = build_report(structured_info, ordering)
     number_of_valid_racers = len([None for racer in prepared_info_for_report if racer.lap_time])
-    racer_number_sequence = iter(range(1, number_of_valid_racers + 1))
-    info_for_output = []
+    if ordering == 'desc':
+        racer_number_sequence = iter(range(number_of_valid_racers, 0, -1))
+    else:
+        racer_number_sequence = iter(range(1, number_of_valid_racers + 1))
     for racer in prepared_info_for_report:
         if racer.lap_time:
-            racers_number = next(racer_number_sequence)
-            info_for_output.append((racers_number, racer.name, racer.team, racer.lap_time_str, racer.abbreviation))
+            racer.place = next(racer_number_sequence)
         else:
-            info_for_output.append(('-', racer.name, racer.team, racer.lap_time_str, racer.abbreviation))
-    return render_template('drivers.html', info_for_output=info_for_output)
-
-
-@app.route('/report/drivers/desc')
-def drivers_desc():
-    structured_info = processing_data('files/start.log', 'files/end.log', 'files/abbreviations.txt')
-    prepared_info_for_report = build_report(structured_info, 'desc')
-    number_of_valid_racers = len([None for racer in prepared_info_for_report if racer.lap_time])
-    racer_number_sequence = iter(range(number_of_valid_racers, 0, -1))
-    info_for_output = []
-    for racer in prepared_info_for_report:
-        if racer.lap_time:
-            racers_number = next(racer_number_sequence)
-            info_for_output.append((racers_number, racer.name, racer.team, racer.lap_time_str, racer.abbreviation))
-        else:
-            info_for_output.append(('-', racer.name, racer.team, racer.lap_time_str, racer.abbreviation))
-    return render_template('drivers_desc.html', info_for_output=info_for_output)
-
-
-@app.route('/report/drivers/<abbreviation>')
-def driver(abbreviation):
-    structured_info = processing_data('files/start.log', 'files/end.log', 'files/abbreviations.txt')
-    return render_template('driver.html', racer=structured_info[abbreviation])
+            racer.place = '-'
+    return prepared_info_for_report
 
 
 if __name__ == '__main__':
